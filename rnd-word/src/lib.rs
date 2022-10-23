474c92afcc5f7be5293303128e620140b95449a2
Kaustubh Agrawal <kaustubhagrawal8838@gmail.com>
2022-10-23 17:15:51 +0530

    wasm-module done

--- rnd-word/src/lib.rs
+++ rnd-word/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod data;
+
+use data::data::get_words;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = Math)]
+    fn random() -> f64;
+
+    #[wasm_bindgen(js_namespace=console)]
+    fn log(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn get_n_words(n: u8) -> String {
+    log("Started executing!!!!");
+    let mut i = 0;
+    let words = get_words();
+    log("Read words!!!!");
+    let word_list: Vec<&str> = words.trim().split('\n').collect();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let mut selected_words = Vec::<String>::new();
+
+    // factor for getting random values bw range
+    let len = word_list.len();
+
+    while i != n {
+        let indx = (random() * len as f64) as usize;
+        let word = word_list.get(indx);
+
+        println!("{} = {:?}", indx, word.clone().to_owned());
+
+        match word {
+            Some(val) => {
+                let value = val.to_string();
+                if selected_words.contains(&value) {
+                    continue;
+                } else {
+                    selected_words.push(value);
+                    i += 1;
+                }
+            }
+            None => continue,
+        }
     }
+
+    println!("{:?}", selected_words);
+    log("Completed!!!!");
+
+    selected_words.join(":")
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		minify: false,
+	},
 	plugins: [wasmPack('./rnd-word'), svelte()],
 });
