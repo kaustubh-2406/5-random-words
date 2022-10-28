173e96f855ee2c19da2cb39f09b7e5f824b239b6
Kaustubh Agrawal <kaustubhagrawal8838@gmail.com>
2022-10-29 01:13:43 +0530

    better asyncstate

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,56 @@
 <script lang="ts">
-  import init, { get_n_words } from 'rnd-word';
-  import { onMount } from 'svelte';
+	import init, { get_n_words } from 'rnd-word';
 
-  let words = [];
-  let ready = false;
-  
-  function handleClick() {
-    if (ready) {
-      words = get_n_words(5).split(':');
-      console.log(words)
-    }
-  }
+	let words = [];
 
-  onMount(async () => {
-    await init()
-    ready = true;
-  })
+	function handleClick() {
+		words = get_n_words(5).split(':');
+		console.log(words);
+	}
 </script>
 
 <main>
-  <div class="mb-4">
-    <h1>5 Random Words</h1>
-    <div>Get five random words for projects some fun project ideas...</div>
-  </div>
+	<div class="mb-4">
+		<h1>5 Random Words</h1>
+		<div>Get five random words for projects some fun project ideas...</div>
+	</div>
 
-  <!-- Show loading untill wasm module is loaded.. -->
-  {#if !ready}
-    <div>Loading....</div>
-  {:else}
-    <button on:click={handleClick}>Get random words</button>
+	<!-- Show loading untill wasm module is loaded.. -->
+	{#await init()}
+		<div>Loading....</div>
+	{:then _}
+		<button on:click={handleClick}>Get random words</button>
 
-    <!-- If words are generated, then show heading -->
-    {#if words.length != 0}
-      <h4>Generated words</h4>
-    {/if}
+		<!-- If words are generated, then show heading -->
+		{#if words.length != 0}
+			<h4>Generated words</h4>
+		{/if}
 
-    <!-- Container div -->
-    <div class="d-flex">
-      {#each words as word}
-        <div class="light">{word}</div>
-      {/each}
-    </div>
-  {/if}
+		<!-- Container div -->
+		<div class="d-flex">
+			{#each words as word}
+				<div class="light">{word}</div>
+			{/each}
+		</div>
+	{/await}
 </main>
 
 <style>
-  .mb-4 {
-    margin-bottom: 2rem;
-  }
+	.mb-4 {
+		margin-bottom: 2rem;
+	}
 
-  .light {
-    color: black;
-    padding: 5px 10px;
-    border-radius: 20px;
-    background-color: rgba(255, 255, 255, 0.5);
-  }
+	.light {
+		color: black;
+		padding: 5px 10px;
+		border-radius: 20px;
+		background-color: rgba(255, 255, 255, 0.5);
+	}
 
-  .d-flex {
-    display: flex;
-    gap: 1rem;
-    justify-content: center;
-    align-items: center;
-  }
+	.d-flex {
+		display: flex;
+		gap: 1rem;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
