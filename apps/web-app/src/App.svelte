cae1675d231c1ef144ed79cae79dd4137dad7331
Kaustubh Agrawal <kaustubhagrawal8838@gmail.com>
2022-11-04 11:12:39 +0530

    ui improvements

--- apps/web-app/src/App.svelte
+++ apps/web-app/src/App.svelte
@@ -6,6 +6,9 @@
 		selected: boolean;
 	};
 
+	type PanelOptions = 'description' | 'details';
+
+	let active: PanelOptions = 'description';
 	let words: Word[] = [];
 
 	function handleClick() {
@@ -19,65 +22,136 @@
 	}
 </script>
 
-<main>
-	<div class="mb-4">
-		<h1>5 Random Words</h1>
-		<div>Get five random words to spark some interesting project ideas..</div>
-	</div>
-
-	<!-- Show loading until wasm module is loaded.. -->
-	{#await init()}
-		<div>Loading....</div>
-	{:then _}
-		<button class="mb-4" on:click={handleClick}>Get random words</button>
-
-		{#if words.length}
-			<div class="d-flex border p-6">
-				<h4>Generated words:</h4>
-
-				{#each words as word}
-					<button
-						class="light"
-						class:selected={word.selected}
-						on:click={() => (word.selected = !word.selected)}
-						>{word.text}</button
+<section
+	class="text-gray-400 bg-gray-900 body-font container px-5 py-24 m-auto"
+>
+	<div>
+		<div class="flex justify-between items-center">
+			<div>
+				<h2 class="text-sm title-font text-gray-500 tracking-widest">
+					<a
+						target="_blank"
+						rel="noreferrer"
+						class="underline"
+						href="https://github.com/kaustubh-2406"
 					>
-				{/each}
+						Kaustubh
+					</a>
+				</h2>
+				<h1 class="text-white text-3xl title-font font-medium mb-4">
+					5 Random Words
+				</h1>
 			</div>
-		{/if}
-	{/await}
-</main>
+
+			<div class="sm:hidden">
+				{#await init()}
+					<div class="text-white">Loading WASM binaries....</div>
+				{:then _}
+					<button
+						class="text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded"
+						on:click={handleClick}
+					>
+						Get random words
+					</button>
+					<!-- {#if words.length}
+						
+					{:else}
+						<div class="w-full">No words selected!</div>
+					{/if} -->
+				{/await}
+			</div>
+		</div>
+		<div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+			<div class="hidden sm:inline-block">
+				<!-- panel -->
+				<div class="flex mb-4">
+					<button
+						class="flex-grow border-gray-800 border-b-2 py-2 text-lg px-1"
+						class:pane-selected={active === 'description'}
+						on:click={() => (active = 'description')}
+					>
+						Description
+					</button>
+					<button
+						class="flex-grow border-b-2 border-gray-800 py-2 text-lg px-1"
+						class:pane-selected={active === 'details'}
+						on:click={() => (active = 'details')}
+					>
+						Details
+					</button>
+				</div>
+
+				<!-- Content -->
+				<div class="leading-relaxed h-60">
+					{#if active === 'description'}
+						<p>
+							Click the following button to get some auto generated
+							suggestions...
+						</p>
+
+						<button
+							class="mt-8 text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded"
+							on:click={handleClick}
+						>
+							Get random words
+						</button>
+					{/if}
+
+					{#if active === 'details'}
+						<p>
+							Hey! You found us. This helper would try and suggest you some
+							words that you might lock by clicking on the suggestion, and then
+							generate another set of random words.
+						</p>
+						<p>
+							You could also take a look at
+							<a
+								target="_blank"
+								class="underline"
+								rel="noopener noreferrer"
+								href="http://www.github.com/kaustubh-2406/5-random-words"
+							>
+								Github repo
+							</a>
+							for cli and upcoming changes.
+						</p>
+					{/if}
+				</div>
+			</div>
+			<div
+				class="h-60 rounded-lg bg-gray-800 bg-opacity-40 flex justify-center items-center"
+			>
+				<div class="justify-center items-center">
+					{#await init()}
+						<div class="text-white">Loading....</div>
+					{:then _}
+						{#if words.length}
+							<div class="flex items-center p-6 gap-2 flex-wrap">
+								{#each words as word}
+									<button
+										class="color-black p-2 px-6 rounded-lg"
+										class:selected={word.selected}
+										on:click={() => (word.selected = !word.selected)}
+									>
+										{word.text}
+									</button>
+								{/each}
+							</div>
+						{:else}
+							<div class="w-full">No words selected!</div>
+						{/if}
+					{/await}
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <style>
-	.mb-4 {
-		margin-bottom: 1rem;
+	.pane-selected {
+		@apply text-purple-400 border-purple-500;
 	}
-
-	.p-6 {
-		padding: 1.5rem;
-	}
-
-	.light {
-		color: black;
-		padding: 5px 10px;
-		border-radius: 20px;
-		background-color: rgba(255, 255, 255, 0.5);
-	}
-
-	.d-flex {
-		display: flex;
-		gap: 1rem;
-		justify-content: center;
-		align-items: center;
-	}
-
 	.selected {
-		background: #ccc;
-		color: black;
-	}
-
-	.border {
-		border: 2px solid gray;
-		border-radius: 25px;
+		@apply bg-green-400 text-black;
 	}
 </style>
--- apps/web-app/svelte.config.js
+++ apps/web-app/svelte.config.js
@@ -1,7 +1,8 @@
+import postcss from 'postcss';
 import sveltePreprocess from 'svelte-preprocess';
 
 export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: [postcss(), sveltePreprocess()],
 };
